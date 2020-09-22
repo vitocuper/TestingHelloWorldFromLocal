38441937f18806ee09cc5787aa53824fb509698e
vitocuper <vitocuper@gmail.com>
2020-09-22 11:28:29 -0300

    agregando segunda linea

--- HelloWorldProjectGit/src/HelloToAll.java
+++ HelloWorldProjectGit/src/HelloToAll.java
@@ -4,6 +4,7 @@ public class HelloToAll {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("heeelouu");
+		System.out.println("segunda linea")
 	}
 
 }
