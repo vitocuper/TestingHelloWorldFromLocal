2957fb5856e8ce71eeddfbfab6cdeef3ad233a99
vitocuper <vitocuper@gmail.com>
2020-09-22 12:04:56 -0300

    comentarios de los comentarios

--- HelloWorldProjectGit/src/HelloToAll.java
+++ HelloWorldProjectGit/src/HelloToAll.java
@@ -4,7 +4,8 @@ public class HelloToAll {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("heeelouu");
-		System.out.println("segunda linea")
+		System.out.println("segunda linea");
+		//comentarios
 	}
 
 }
